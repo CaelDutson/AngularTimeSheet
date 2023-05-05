daec89a05cf7c556827e56bbe07e6639502f764a
CaelDutson <cael.dutson7714@stu.mtec.edu>
2023-05-05 10:11:31 -0600

    changes

---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './modules/app-routing.module';
+import { AppComponent } from './app.component';
+import { InterfacesComponent } from './interfaces/interfaces.component';
+
+import { ServicesComponent } from './services/services.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DepartmentsComponent } from './components/departments/departments.component';
+import { TimesheetComponent } from './components/timesheet/timesheet.component';
+import { AnalyticsComponent } from './components/analytics/analytics.component';
+import { TopNavbarComponent } from './components/top-navbar/top-navbar.component';
+import { AnalyticsTableComponent } from './components/analytics-table/analytics-table.component'; 
+import { MaterialModule } from './modules/material.module';  
+import { FormsModule } from '@angular/forms'; 
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InterfacesComponent,
+    ServicesComponent,
+    DepartmentsComponent,
+    TimesheetComponent,
+    AnalyticsComponent,
+    TopNavbarComponent,
+    AnalyticsTableComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule, 
+    MaterialModule, 
+    FormsModule, 
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/analytics/analytics.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'; 
+import { Employee } from 'src/app/interfaces/employee';
+
+@Component({
+  selector: 'app-analytics',
+  templateUrl: './analytics.component.html',
+  styleUrls: ['./analytics.component.scss']
+})
+export class AnalyticsComponent { 
+  
+}
